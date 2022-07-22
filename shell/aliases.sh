e4d01d574750c522daab94002e0b34b1ff82c081
Antonio Ferrer Lopez <buceocv@gmail.com>
2022-07-22 11:23:54 +0200

    Añadiendo config antigua y configurado 10k con zim

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,8 +3,8 @@ alias sudo='sudo '
 
 alias ..="cd .."
 alias ...="cd ../.."
-alias ll="ls -l"
-alias la="ls -la"
+alias ll="exa -l"
+alias la="exa -la"
 alias ~="cd ~"
 alias dotfiles='cd $DOTFILES_PATH'
 
@@ -13,6 +13,7 @@ alias gaa="git add -A"
 alias gc='$DOTLY_PATH/bin/dot git commit'
 alias gca="git add --all && git commit --amend --no-edit"
 alias gco="git checkout"
+alias gcob="git checkout -b"
 alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
 alias gs="git status -sb"
 alias gf="git fetch --all -p"
@@ -21,10 +22,43 @@ alias gpsf="git push --force"
 alias gpl="git pull --rebase --autostash"
 alias gb="git branch"
 alias gl='$DOTLY_PATH/bin/dot git pretty-log'
+alias gcm='git commit -m '
+alias gcdev='git checkout development'
+
 
 # Utils
 alias k='kill -9'
+alias xk='xkill'
 alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
 alias o.='open .'
 alias up='dot package update_all'
+alias aliasmod="code $DOTLY_PATH/../../shell/aliases.sh "
+alias aliasgrep="alias | grep"
+alias postman="/home/antonio/Postman/./Postman"
+
+
+#Docker
+alias doconn="dot docker connect.sh"
+alias dostop="docker stop"
+alias dorm="docker rm"
+
+
+#Editors
+alias code="snap run code"
+alias p.="~/.dotfiles/editors/./phpstorm "
+alias d.="~/.dotfiles/editors/./datagrip "
+alias w.="~/.dotfiles/editors/./webstorm "
+
+#Personal 
+alias proman="cd /home/antonio/projects/tci/promanager"
+
+
+#Symfony
+alias syreq="symfony composer req"
+alias syreqdev="symfony composer req --dev"
+alias syserv="symfony serve"
+alias syservstop="symfony serve:stop"
+alias sycon="symfony console"
+alias sycom="symfony composer"
+alias sytest="php bin/phpunit"
--- shell/zsh/.zshrc
+++ shell/zsh/.zshrc
@@ -1,3 +1,10 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 #!/usr/bin/env zsh
 # Uncomment for debuf with `zprof`
 # zmodload zsh/zprof
@@ -12,6 +19,7 @@ ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 
 # Start Zim
 source "$ZIM_HOME/init.zsh"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Async mode for autocompletion
 ZSH_AUTOSUGGEST_USE_ASYNC=true
@@ -19,11 +27,16 @@ ZSH_HIGHLIGHT_MAXLENGTH=300
 
 source "$DOTFILES_PATH/shell/init.sh"
 
-fpath=("$DOTFILES_PATH/shell/zsh/themes" "$DOTFILES_PATH/shell/zsh/completions" "$DOTLY_PATH/shell/zsh/themes" "$DOTLY_PATH/shell/zsh/completions" $fpath)
+export fpath=("$DOTFILES_PATH/shell/zsh/themes" "$DOTLY_PATH/shell/zsh/completions" $fpath)fpath=("$DOTFILES_PATH/shell/zsh/themes" "$DOTFILES_PATH/shell/zsh/completions" "$DOTLY_PATH/shell/zsh/themes" "$DOTLY_PATH/shell/zsh/completions" $fpath)
 
 autoload -Uz promptinit && promptinit
 prompt ${DOTLY_THEME:-codely}
+#prompt zsh
 
 source "$DOTLY_PATH/shell/zsh/bindings/dot.zsh"
 source "$DOTLY_PATH/shell/zsh/bindings/reverse_search.zsh"
 source "$DOTFILES_PATH/shell/zsh/key-bindings.zsh"
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+(( ! ${+functions[p10k]} )) || p10k finalize
